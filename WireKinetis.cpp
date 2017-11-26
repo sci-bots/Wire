6e74a477e581ee08235434a1dbb5127bc676f8b4
Christian Fobel <christian@fobel.net>
2017-11-26 17:31:59 -0500

    fix(teensy): fix I2C_Hardware_t attribute const

--- WireKinetis.cpp
+++ WireKinetis.cpp
@@ -729,7 +729,7 @@ uint8_t twi_writeTo(uint8_t address, uint8_t* data, uint8_t length, uint8_t wait
 	return Wire.endTransmission(sendStop);
 }
 
-TwoWire::I2C_Hardware_t TwoWire::i2c0_hardware = {
+const TwoWire::I2C_Hardware_t TwoWire::i2c0_hardware = {
 	SIM_SCGC4, SIM_SCGC4_I2C0,
 #if defined(__MKL26Z64__) || defined(__MK20DX128__) || defined(__MK20DX256__)
 	18, 17, 255, 255, 255,
@@ -746,7 +746,7 @@ TwoWire::I2C_Hardware_t TwoWire::i2c0_hardware = {
 };
 
 #if defined(__MKL26Z64__) || defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__)
-TwoWire::I2C_Hardware_t TwoWire::i2c1_hardware = {
+const TwoWire::I2C_Hardware_t TwoWire::i2c1_hardware = {
 	SIM_SCGC4, SIM_SCGC4_I2C1,
 #if defined(__MKL26Z64__)
 	23, 255, 255, 255, 255,
@@ -769,7 +769,7 @@ TwoWire::I2C_Hardware_t TwoWire::i2c1_hardware = {
 #endif
 
 #if defined(__MK64FX512__) || defined(__MK66FX1M0__)
-TwoWire::I2C_Hardware_t TwoWire::i2c2_hardware = {
+const TwoWire::I2C_Hardware_t TwoWire::i2c2_hardware = {
 	SIM_SCGC1, SIM_SCGC1_I2C2,
 #if defined(__MK64FX512__) || defined(__MK66FX1M0__)
 	4, 255, 255, 255, 255,
@@ -782,7 +782,7 @@ TwoWire::I2C_Hardware_t TwoWire::i2c2_hardware = {
 #endif
 
 #if defined(__MK66FX1M0__)
-TwoWire::I2C_Hardware_t TwoWire::i2c3_hardware = {
+const TwoWire::I2C_Hardware_t TwoWire::i2c3_hardware = {
 	SIM_SCGC1, SIM_SCGC1_I2C3,
 #if defined(__MK66FX1M0__)
 	56, 255, 255, 255, 255,
@@ -800,22 +800,22 @@ TwoWire::I2C_Hardware_t TwoWire::i2c3_hardware = {
 #define MAKE_CONST(x) (__builtin_constant_p(x) ? (x) : (x))
 
 #ifdef WIRE_IMPLEMENT_WIRE
-constexpr uintptr_t i2c0_addr = uintptr_t(MAKE_CONST(&KINETIS_I2C0));
+const uintptr_t i2c0_addr = uintptr_t(MAKE_CONST(&KINETIS_I2C0));
 TwoWire Wire(i2c0_addr, TwoWire::i2c0_hardware);
 void i2c0_isr(void) { Wire.isr(); }
 #endif
 #ifdef WIRE_IMPLEMENT_WIRE1
-constexpr uintptr_t i2c1_addr = uintptr_t(MAKE_CONST(&KINETIS_I2C1));
+const uintptr_t i2c1_addr = uintptr_t(MAKE_CONST(&KINETIS_I2C1));
 TwoWire Wire1(i2c1_addr, TwoWire::i2c1_hardware);
 void i2c1_isr(void) { Wire1.isr(); }
 #endif
 #ifdef WIRE_IMPLEMENT_WIRE2
-constexpr uintptr_t i2c2_addr = uintptr_t(MAKE_CONST(&KINETIS_I2C2));
+const uintptr_t i2c2_addr = uintptr_t(MAKE_CONST(&KINETIS_I2C2));
 TwoWire Wire2(i2c2_addr, TwoWire::i2c2_hardware);
 void i2c2_isr(void) { Wire2.isr(); }
 #endif
 #ifdef WIRE_IMPLEMENT_WIRE3
-constexpr uintptr_t i2c3_addr = uintptr_t(MAKE_CONST(&KINETIS_I2C3));
+const uintptr_t i2c3_addr = uintptr_t(MAKE_CONST(&KINETIS_I2C3));
 TwoWire Wire3(i2c3_addr, TwoWire::i2c3_hardware);
 void i2c3_isr(void) { Wire3.isr(); }
 #endif
